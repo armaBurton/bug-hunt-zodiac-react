2ab0d95d91136420b7e6d244e9d0370d9ae3226c
armaBurton <armanarma@armaburton.com>
2022-01-25 14:53:12 -0800

    all bugs fixed

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,10 +3,10 @@ import ZodiacCard from '../ZodiacCard/ZodiacCard.js'
 import backgroundImg from '../../background.png'
 import './Main.css'
 
-export default function Main() {
+export default function Main(props) {
   return (
     <main style={{ backgroundImage: `url(${backgroundImg})` }}>
-      {zodiac.map((item) => (
+      {props.zodiac.map((item) => (
         <ZodiacCard key={item.name} {...item} />
       ))}
     </main>
